eed0c2ee2bd2572a99072dee7c76313547c0c2b0
UsenUtebayev <usenutebayev@gmail.com>
2023-03-21 17:44:46 +0500

    -feat: now error message in forms has custom css class

--- src/forms.py
+++ src/forms.py
@@ -2,15 +2,32 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.forms import ModelForm
+from django.forms.utils import ErrorList
 
 from src.models import Task
 
 
+class CustomErrorList(ErrorList):
+    def get_context(self):
+        return {
+            "errors": self,
+            "error_class": "alert alert-danger h6 list-unstyled",
+        }
+
+
 class UserRegistrationForm(UserCreationForm):
-    username = forms.CharField(label='Имя пользвателя', widget=forms.TextInput(attrs={"class": "form-control"}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={"class": "form-control"}))
-    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={"class": "form-control"}))
-    email = forms.EmailField(label='Ваша почта', widget=forms.EmailInput(attrs={"class": "form-control"}))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.error_class = CustomErrorList
+
+    error_messages = {
+        "password_mismatch": "Пароли не совпадают"
+    }
+    attrs = {"class": "form-control h6"}
+    username = forms.CharField(label='Имя пользвателя', widget=forms.TextInput(attrs=attrs))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs=attrs))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs=attrs))
+    email = forms.EmailField(label='Ваша почта', widget=forms.EmailInput(attrs=attrs))
 
     class Meta:
         model = User
@@ -18,8 +35,23 @@ class UserRegistrationForm(UserCreationForm):
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Имя пользвателя', widget=forms.TextInput(attrs={"class": "form-control"}))
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.error_class = CustomErrorList
+
+    username_label = "Имя пользвателя"
+    passwors_label = "Пароль"
+    username_attrs = {"class": "form-control", "placeholder": username_label}
+    password_attrs = {"class": "form-control mt-3", "placeholder": passwors_label}
+    username = forms.CharField(label='', widget=forms.TextInput(attrs=username_attrs))
+    password = forms.CharField(label='', widget=forms.PasswordInput(attrs=password_attrs))
+    error_messages = {
+        "invalid_login": (
+            "Вы ошиблись паролем, логином, или у вас еще не создан аккаунт."
+            " Поля чувствительны к регистру будьте осторожнее."
+        ),
+        "inactive": "Аккаунт был забанен",
+    }
 
 
 class TaskCreationForm(ModelForm):
